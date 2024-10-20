5b2711324931e7c16b3f7b222fe845d05f5b0b24
AKADortys <thibault.a@outlook.com>
2024-10-20 14:43:11 +0200

    Ajout possibilité de masquer la barre de navigation

--- JS/header.js
+++ JS/header.js
@@ -1,12 +1,33 @@
 const list_header = document.getElementById('header-list');
 const section_main = document.querySelectorAll('.contenue');
 const title = document.getElementById('title');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const hidde_navbar = document.getElementById('hidde_navbar');
+let navbarIsOpen = true;
+
+hidde_navbar.addEventListener('click', function(){
+    if(navbarIsOpen){
+        header.style.transform = 'translateY(-20vh)';
+        setTimeout(function(){
+            header.style.display = 'none';
+        }, 200)
+        navbarIsOpen = false;
+    } else {
+        header.style.display = 'flex';
+        setTimeout(function(){
+            header.style.transform = 'translateY(0vh)';
+        }, 50)
+        navbarIsOpen = true;
+    }
+})
 
 
 function closeSections(){
     section_main.forEach(function(section){
         section.style.display = 'none';
         section.style.opacity = '0';
+        title.style.display = 'block';
         Array.from(list_header.children).forEach(function(element) {
             element.style.backgroundColor ="";
             element.style.border = "";
