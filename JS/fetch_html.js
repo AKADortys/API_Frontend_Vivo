169a688036f9d08afb46344a74a5610461216140
AKADortys <thibault.a@outlook.com>
2024-10-23 09:32:02 +0200

    Changement de l'organisation du fichier html avec fetch. fragmentation en partiales

---
+++ JS/fetch_html.js
@@ -0,0 +1,28 @@
+// Charger les fichiers partiels HTML
+Promise.all([
+    fetch("Partials/newsletter.html").then(response => response.text()),
+    fetch("Partials/header.html").then(response => response.text()),
+    fetch("Partials/main.html").then(response => response.text()),
+    fetch("Partials/footer.html").then(response => response.text())
+  ])
+  .then(([newsletter, header, main, footer]) => {
+    document.getElementById("newslettre").innerHTML = newsletter;
+    document.querySelector("header").innerHTML = header;
+    document.querySelector("main").innerHTML = main;
+    document.querySelector("footer").innerHTML = footer;
+    
+    // Charger les autres scripts après l'insertion HTML
+    loadAdditionalScripts();
+  });
+  
+  function loadAdditionalScripts() {
+    const scriptsToLoad = ['JS/header.js', 'JS/newslettre.js'];
+  
+    scriptsToLoad.forEach(src => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.defer = true;
+      document.body.appendChild(script);
+    });
+  }
+  
