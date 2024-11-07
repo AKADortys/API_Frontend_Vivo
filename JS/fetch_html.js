ae0f350b870efbc2b69ec7dc3d5578e353966de5
AKADortys <thibault.a@outlook.com>
2024-11-07 19:37:42 +0100

    factorisation du fichier main.html

--- JS/fetch_html.js
+++ JS/fetch_html.js
@@ -1,15 +1,29 @@
 export async function loadPartials() {
   // Charger les fichiers partiels HTML
-  const [newsletter, header, main, footer] = await Promise.all([
-    fetch("Partials/newsletter.html").then(response => response.text()),
-    fetch("Partials/header.html").then(response => response.text()),
-    fetch("Partials/main.html").then(response => response.text()),
-    fetch("Partials/footer.html").then(response => response.text())
+  const [
+    newsletter,
+    header,
+    accueil,
+    connexion,
+    contact,
+    produit,
+    panier,
+    footer,
+  ] = await Promise.all([
+    fetch("Partials/newsletter.html").then((response) => response.text()),
+    fetch("Partials/header.html").then((response) => response.text()),
+    fetch("Partials/accueil-section.html").then((response) => response.text()),
+    fetch("Partials/connexion-section.html").then((response) =>response.text()),
+    fetch("Partials/contact-section.html").then((response) => response.text()),
+    fetch("Partials/produit-section.html").then((response) => response.text()),
+    fetch("Partials/panier-section.html").then((response) => response.text()),
+    fetch("Partials/footer.html").then((response) => response.text()),
   ]);
 
   document.getElementById("newslettre").innerHTML = newsletter;
   document.querySelector("header").innerHTML = header;
-  document.querySelector("main").innerHTML = main;
+  document.querySelector("main").innerHTML =
+    accueil + connexion + contact + produit + panier;
   document.querySelector("footer").innerHTML = footer;
 
   console.log("Partials chargées");
